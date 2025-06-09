4399bd824d6efa2b9229a5b8159b637858634ae2
Maicon Gretschmann <mr.gretschmann@gmail.com>
2025-06-09 20:13:27 -0300

    Atualização CORS

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// Configuração do middleware de CORS
+app.use(
+  cors({
+    origin: "https://maiconrg.github.io", // Permita somente seu front-end
+    methods: ["GET"], // Permita apenas requisições GET
+    credentials: true, // Configure se houver cookies ou autenticação
+  })
+);
+
 app.use(express.json());
 
+// Conexão com o MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -19,12 +28,14 @@ mongoose
     console.error("Erro ao conectar ao MongoDB:", err.message);
   });
 
+// Modelo do contador
 const visitSchema = new mongoose.Schema({
   count: { type: Number, default: 0 },
 });
 
 const Visit = mongoose.model("Visit", visitSchema);
 
+// Rotas
 app.get("/", (req, res) => {
   res.send("Servidor backend rodando!");
 });
@@ -47,6 +58,7 @@ app.get("/contador", async (req, res) => {
   }
 });
 
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
